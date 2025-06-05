f413b3fd7e093c28d3fcc3ec0646768ade702d7e
Paxton Nguyen <158031346+ntnquang2906@users.noreply.github.com>
2025-06-05 20:21:36 +1000

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,27 @@
+/*-----------------------------------
+Student name: Tien Nhat Quang Nguyen
+Student number: 7722242
+Subject code: CSIT213
+-----------------------------------*/
+
+public class Admin extends Employee {
+    private String skills;
+
+    public Admin(int empNumber, String name, String dob, String address, String gender, double salary, int supervisorNumber, int deptNumber, String skills) {
+        super(empNumber, name, dob, address, gender, salary, supervisorNumber, deptNumber);
+        this.skills = skills;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+
+    @Override
+    public String toString() {
+        return "A, " + getEmpNumber() + ", " + getName() + ", " + getDob() + ", " + getAddress() + ", " + getGender() + ", " + String.format("%.1f", getSalary()) + ", " + getSupervisorNumber() + ", " + getDeptNumber() + (skills.trim().isEmpty() ? "" : ", " + skills.trim());
+    }
+}
